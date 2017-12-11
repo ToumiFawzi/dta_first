8f3c8381fe875fd67f4f381188aaeae0759e72d2
Ftoumi <fawzi.t@hotmail.fr>
2017-12-11 10:49:28 +0100

    premier commit

---
+++ index.php
@@ -0,0 +1,14 @@
+<!Doctype html>
+<html>
+    <head>
+        <meta charset="uft-8">
+        <title> Premier pas avec GIT</title>
+        <link href="styles/style.css" rel="stylesheet" type="text/css">
+    </head>
+
+    <body>
+    <p> blablablabla
+        </p>
+    
+    </body>
+</html>
